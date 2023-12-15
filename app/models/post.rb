6619e7addebd3a16ffa0acf03d1ac249298e7491
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-15 09:13:00 +0100

    Make changes based on post, comment and like files in app/models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,16 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
 end
