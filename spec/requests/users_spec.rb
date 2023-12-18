50dd8e4572fcd50ce32d949ee83ad6635289c910
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-18 17:29:02 +0100

    Add test users_spec.rb file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe 'GET /show' do
-    it 'returns http success' do
+    it 'returns http not found' do
       get '/users/show'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:not_found)
     end
+
+    it 'returns http no content when not acceptable' do
+      user = User.create(name: 'Example User')
+      get "/users/show", params: { id: user.id }, headers: { 'Accept' => 'application/json' }
+      expect(response).to have_http_status(:no_content)
+    end
+    
   end
 end
